0b83b988720ca9eb18ef9e96e2c19531cf0f4776
suhadev <suhadevvenkatesh@gmail.com>
2019-06-18 18:24:37 +0530

    fixed adding Game over part

--- script.js
+++ script.js
@@ -2,48 +2,65 @@ let randomNumber
 let circleClicked = false
 let selectedCircle
 let nextLineCounter = 0
+let selectedElement
+let rowCounter = 1
 function generateRandomNumber(max, min) {
     randomNumber = (Math.floor(Math.random() * (max - min) + min)).toString().split('')
 
-    // console.log(randomNumber)
+     console.log(randomNumber)
 
 }
 generateRandomNumber(1000, 2000)
 
 function selectCircle(e,index) {
+    console.log(e.target)
     if(circleClicked == false) {
         e.target.classList.add("selectedCircle")
         circleClicked = true
+        selectedElement = e.target
         selectedCircle = index
     }
 }
 function detectKey(event) {
-    document.getElementById(selectedCircle).innerHTML = event.key
+    selectedElement.innerHTML = event.key
     let valueEntered = event.key
     // console.log(randomNumber.indexOf(valueEntered))
     if(randomNumber.indexOf(valueEntered)=== selectedCircle) {
         // console.log("CorrectNumber and location")
-        document.getElementById(selectedCircle).classList.add("correctNumber-Position")
+        selectedElement.classList.add("correctNumber-Position")
     } else if(randomNumber.indexOf(valueEntered)!== -1){
         // console.log("correct number wron position")
-        document.getElementById(selectedCircle).classList.add("correctNumber-wrongPosition")
+        selectedElement.classList.add("correctNumber-wrongPosition")
     } else{
         // console.log("wrong Number")
-        document.getElementById(selectedCircle).classList.add("wrongNumber")
+        selectedElement.classList.add("wrongNumber")
     }
-    document.getElementById(selectedCircle).classList.remove("selectedCircle")
+    selectedElement.classList.remove("selectedCircle")
     circleClicked = false
     nextLineCounter ++
-    if(nextLineCounter >= 4) {
+    console.log(nextLineCounter)
+    console.log(rowCounter)
+    
+    if(nextLineCounter == 4 && rowCounter <= 4) {
+        rowCounter++
+        console.log("next")
         document.getElementById("gameArea").innerHTML += `
-        <div class="col-lg-12">
-        <div class="row text-center">
-                <div id="0" class="col-lg-1 circle-bg offset-md-4" onclick="selectCircle(event,0)">1</div>
-                <div id="1" class="col-lg-1 circle-bg" onclick="selectCircle(event,1)">2</div>
-                <div id="2" class="col-lg-1 circle-bg" onclick="selectCircle(event,2)">3</div>
-                <div id="3" class="col-lg-1 circle-bg" onclick="selectCircle(event,3)">4</div>
-        </div>
-    </div>`
+        <div class="col-lg-12 text-center" style="margin-left:320px; margin-top:20px;">
+                    <div class="row text-center">
+                            <div id="0" class="col-lg-1 circle-bg offset-md-6" onclick="selectCircle(event,0)">1</div>
+                            <div id="1" class="col-lg-1 circle-bg" onclick="selectCircle(event,1)">2</div>
+                            <div id="2" class="col-lg-1 circle-bg" onclick="selectCircle(event,2)">3</div>
+                            <div id="3" class="col-lg-1 circle-bg" onclick="selectCircle(event,3)">4</div>
+                    </div>
+                </div>`
     nextLineCounter = 0
+    
+    console.log(rowCounter)
+    }
+    if(rowCounter > 4) {
+        //alert("Game Over")
+        document.getElementById("instruction").innerHTML = "<h2>Game Over!</h2>"
+        document.getElementById("instruction").classList.add("gameOver")
+        nextLineCounter ++
     }
 }
